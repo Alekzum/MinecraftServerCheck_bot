be48aefb163edd213211bb56c7d28ccc431dda5d
Alekzum <68947902+Alekzum@users.noreply.github.com>
2024-05-08 15:36:32 +0400

    add parsing, cooldiwn middleware (aka anti-spam), "i love mypy <3"

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
-from aiogram_sqlite_storage.sqlitestore import SQLStorage
+from aiogram_sqlite_storage.sqlitestore import SQLStorage  # type: ignore
+from aiogram.client.default import DefaultBotProperties
 from aiogram import Bot, Dispatcher
 from utils.config import TOKEN
 from utils.scripts import include_routers
+from utils.middleware import CooldownMiddleware
 import asyncio
 
 
-bot = Bot(token=TOKEN)
+bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode="html"))
 dp = Dispatcher(storage=SQLStorage())
 include_routers(dp)
+
+dp.message.middleware(CooldownMiddleware())
+dp.callback_query.middleware(CooldownMiddleware())
 
 asyncio.run(dp.start_polling(bot))
